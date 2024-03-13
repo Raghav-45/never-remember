85afcb82963e4aaa13605d0b326d7fbcf86b738f
Aditya <raghavbhai4545@gmail.com>
2024-03-13 22:22:00 +0530

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { SearchIcon } from "lucide-react";
+import { PlusIcon, SearchIcon } from "lucide-react";
 
 export default function DashboardLayout({
   children, // will be a page or nested layout
@@ -27,6 +27,13 @@ export default function DashboardLayout({
             </CardHeader>
             <CardContent className="px-0 pt-3">
               <div className="top-0 z-10 sticky flex items-center space-x-2 backdrop-blur-md px-6 py-3">
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="flex-none rounded-full w-9 h-9"
+                >
+                  <PlusIcon className="w-4 h-4" />
+                </Button>
                 <Input
                   type="search"
                   placeholder="Search..."
