5e9c8f84f05b224d2c32f9be3ad15ba8310a0947
Aditya <raghavbhai4545@gmail.com>
2024-03-14 16:30:17 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,32 @@
-import PasswordItem from "@/components/PasswordItem"
+import PasswordItem from "@/components/PasswordItem";
+import { getUserLoginDetails } from "@/lib/dbUtils";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const userLoginDetails = await getUserLoginDetails();
+  console.log(userLoginDetails);
   return (
     <>
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
-      <PasswordItem name='Gmail' url='mail.google.com' image='g' login='adi4545aditya@gmail.com' password='testpass' />
+      {userLoginDetails &&
+      userLoginDetails.length > 0 &&
+      userLoginDetails[0]?.contents.length > 0 ? (
+        userLoginDetails[0].contents.map((elem) => (
+          <PasswordItem
+            name={elem.name}
+            url={elem.url}
+            image={elem.image}
+            login={elem.login}
+            password={elem.password}
+          />
+        ))
+      ) : (
+        <PasswordItem
+          name="Preview"
+          url="never-remember.vercel.app"
+          image="g"
+          login="example@mail.com"
+          password="SuperSecretPassword"
+        />
+      )}
     </>
-  )
+  );
 }
