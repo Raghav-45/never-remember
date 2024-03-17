1b3d845d16ef8f05dd7180f05026c092422d31e9
Aditya <raghavbhai4545@gmail.com>
2024-03-17 15:00:43 +0530

    server side auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+function getGithubCredentials() {
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+  if (!clientId || clientId.length === 0) {
+    throw new Error("Missing GITHUB_CLIENT_ID");
+  }
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error("Missing GITHUB_CLIENT_SECRET");
+  }
+  return { clientId, clientSecret };
+}
+
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: getGithubCredentials().clientId,
+      clientSecret: getGithubCredentials().clientSecret,
+    }),
+  ],
+} satisfies NextAuthOptions;
