6db5fcaa235e341fb864ee631b8352d8a9d539cd
Aditya <raghavbhai4545@gmail.com>
2024-03-19 00:16:44 +0530

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,6 +7,18 @@ import { getUserLoginDetails } from '@/lib/dbUtils'
 import { getServerSession } from 'next-auth'
 import { getSession } from 'next-auth/react'
 
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  Card,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { PlusIcon, SearchIcon } from 'lucide-react'
+import NewEntryDialog from '@/components/NewEntryDialog'
+
 export default function Dashboard() {
   const { userLoginDetails, setUserLoginDetails } = useGenerationStore()
 
@@ -19,29 +31,56 @@ export default function Dashboard() {
   }
   getUserDetails()
   return (
-    <>
-      {userLoginDetails &&
-      userLoginDetails.length > 0 &&
-      userLoginDetails[0]?.contents.length > 0 ? (
-        userLoginDetails[0].contents.map((elem) => (
-          <PasswordItem
-            key={elem.url}
-            name={elem.name}
-            url={elem.url}
-            image={elem.image}
-            login={elem.login}
-            password={elem.password}
+    <Card>
+      <CardHeader className="pb-0">
+        <CardTitle>Passwords</CardTitle>
+        <CardDescription>
+          You don&apos;t have to remember 235 passwords.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="px-0 pt-3">
+        <div className="top-0 z-10 sticky flex items-center space-x-2 backdrop-blur-md px-6 py-3">
+          <NewEntryDialog />
+          <Input
+            type="search"
+            placeholder="Search..."
+            className="flex-auto w-full"
           />
-        ))
-      ) : (
-        <PasswordItem
-          name="Preview"
-          url="never-remember.vercel.app"
-          image="g"
-          login="example@mail.com"
-          password="SuperSecretPassword"
-        />
-      )}
-    </>
+          <Button
+            variant="outline"
+            size="icon"
+            className="flex-none rounded-full w-9 h-9"
+          >
+            <SearchIcon className="w-4 h-4" />
+          </Button>
+        </div>
+        <div className="space-y-8 px-6 pt-3">
+          {userLoginDetails &&
+          userLoginDetails.length > 0 &&
+          userLoginDetails[0]?.contents.length > 0 && (
+            userLoginDetails[0].contents.map((elem) => (
+              <PasswordItem
+                key={elem.url}
+                name={elem.name}
+                url={elem.url}
+                image={elem.image}
+                login={elem.login}
+                password={elem.password}
+              />
+            ))
+          )
+          // : (
+          //   <PasswordItem
+          //     name="Preview"
+          //     url="never-remember.vercel.app"
+          //     image="g"
+          //     login="example@mail.com"
+          //     password="SuperSecretPassword"
+          //   />
+          // )
+          }
+        </div>
+      </CardContent>
+    </Card>
   )
 }
