5cd4c2411cca76dca6608d61ee2c6f2ac34c2f41
Aditya <raghavbhai4545@gmail.com>
2024-03-19 23:53:35 +0530

    homepage

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -16,37 +16,39 @@ export default function LoginLayout({
   children: React.ReactNode
 }) {
   return (
-    <Card>
-      <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl">Create an account</CardTitle>
-        <CardDescription>
-          Enter your email below to create your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="gap-4 grid">
-        {children}
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="border-t w-full" />
+    <div className='flex justify-center items-center h-svh'>
+      <Card className='shadow-lg'>
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl">Create an account</CardTitle>
+          <CardDescription>
+            Enter your email below to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="gap-4 grid">
+          {children}
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <span className="border-t w-full" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                Or continue with
+              </span>
+            </div>
           </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Or continue with
-            </span>
+          <div className="gap-2 grid">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" />
           </div>
-        </div>
-        <div className="gap-2 grid">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" />
-        </div>
-        <div className="gap-2 grid">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Create account</Button>
-      </CardFooter>
-    </Card>
+          <div className="gap-2 grid">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Create account</Button>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,184 @@
-import Image from "next/image";
+import { Button, buttonVariants } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { CheckIcon, MountainIcon, PlusIcon } from 'lucide-react'
+import Link from 'next/link'
+
+const IconWithText = ({ text }: { text: string }) => {
+  return (
+    <li className="flex items-center space-y-1">
+      <CheckIcon className="inline-block mr-2 w-4 h-4" />
+      <div className="font-semibold text-sm md:text-md lg:text-lg">{text}</div>
+    </li>
+  )
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex flex-col">
+      <header className="flex items-center px-4 lg:px-6 h-14">
+        <Link className="flex justify-center items-center" href="#">
+          <MountainIcon className="w-6 h-6" />
+          <span className="sr-only">Acme Inc</span>
+        </Link>
+        <nav className="flex items-center gap-4 sm:gap-6 ml-auto">
+          <Link
+            className="font-medium text-sm underline-offset-4 hover:underline"
+            href="#"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            Features
+          </Link>
+          <Link
+            className="font-medium text-sm underline-offset-4 hover:underline"
+            href="#"
+          >
+            About
+          </Link>
+          <Link
+            className={buttonVariants({ variant: 'default' })}
+            href="/login"
+          >
+            Login
+          </Link>
+        </nav>
+      </header>
+      <main className="flex-1">
+        <section className="py-6 sm:py-12 md:py-24 lg:py-32 xl:py-48 w-full">
+          <div className="px-4 md:px-6 container">
+            <div className="gap-6 lg:gap-12 grid lg:grid-cols-[1fr_400px] xl:grid-cols-[1fr_600px]">
+              <div className="flex flex-col justify-center space-y-4">
+                <div className="space-y-2">
+                  <h1 className="font-bold text-3xl sm:text-5xl xl:text-6xl/none tracking-tighter">
+                    Secure your digital life with our Password Manager
+                  </h1>
+                  <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                    Access your passwords securely across all your devices.
+                    Never forget a password again.
+                  </p>
+                </div>
+                <div className="min-[400px]:flex-row">
+                  <Link
+                    className="inline-flex justify-center items-center bg-gray-900 hover:bg-gray-900/90 dark:hover:bg-gray-50/90 dark:bg-gray-50 disabled:opacity-50 shadow px-8 rounded-md h-10 font-medium text-gray-50 text-sm dark:text-gray-900 transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none dark:focus-visible:ring-gray-300"
+                    href="/login"
+                  >
+                    Get Started
+                  </Link>
+                </div>
+              </div>
+              <img
+                alt="Hero"
+                className="lg:order-last mx-auto rounded-xl sm:w-full overflow-hidden aspect-video object-center object-cover lg:aspect-square"
+                height="550"
+                src="https://generated.vusercontent.net/placeholder.svg"
+                width="550"
+              />
+            </div>
+          </div>
+        </section>
+        <section className="py-12 md:py-24 lg:py-32 w-full">
+          <div className="items-center gap-6 lg:gap-10 grid lg:grid-cols-2 px-4 md:px-6 container">
+            <div className="space-y-2">
+              <h2 className="font-bold text-3xl md:text-4xl/tight tracking-tighter">
+                Features
+              </h2>
+              <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed xl:text-base/relaxed dark:text-gray-400">
+                Our password manager is packed with features to keep your
+                passwords secure and your life simple.
+              </p>
+            </div>
+            <div className="items-start gap-6 grid mx-auto max-w-5xl">
+              <div className="gap-1 grid">
+                <div className="inline-flex items-center">
+                  <CheckIcon className="mr-2 w-4 h-4" />
+                  <h3 className="font-bold text-lg">
+                    Secure password generation
+                  </h3>
+                </div>
+                <p className="text-gray-500 text-sm dark:text-gray-400">
+                  Automatically create strong, unique passwords for every site.
+                </p>
+              </div>
+              <div className="gap-1 grid">
+                <div className="inline-flex items-center">
+                  <CheckIcon className="mr-2 w-4 h-4" />
+                  <h3 className="font-bold text-lg">Secure password storage</h3>
+                </div>
+                <p className="text-gray-500 text-sm dark:text-gray-400">
+                  Your passwords are encrypted and only accessible to you.
+                </p>
+              </div>
+              <div className="gap-1 grid">
+                <div className="inline-flex items-center">
+                  <PlusIcon className="mr-2 w-4 h-4 rotate-45" />
+                  <h3 className="font-bold text-lg">Auto-fill functionality</h3>
+                </div>
+                <p className="text-gray-500 text-sm dark:text-gray-400">
+                  Automatically fill in your passwords on websites with the
+                  click of a button.
+                </p>
+              </div>
+              <div className="gap-1 grid">
+                <div className="inline-flex items-center">
+                  <CheckIcon className="mr-2 w-4 h-4" />
+                  <h3 className="font-bold text-lg">Cross-device sync</h3>
+                </div>
+                <p className="text-gray-500 text-sm dark:text-gray-400">
+                  Access your passwords on all your devices, wherever you are.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="py-12 md:py-24 lg:py-32 border-t w-full">
+          <div className="px-4 md:px-6 container">
+            <div className="gap-10 md:gap-16 grid md:grid-cols-2 sm:px-10">
+              <div className="space-y-4">
+                <div className="inline-block bg-gray-100 dark:bg-gray-800 px-3 py-1 rounded-lg text-sm">
+                  Performance
+                </div>
+                <h2 className="font-bold text-3xl sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem] lg:leading-tighter tracking-tighter">
+                  Traffic spikes should be exciting, not scary.
+                </h2>
+                <Link
+                  className="inline-flex justify-center items-center bg-gray-900 hover:bg-gray-900/90 dark:hover:bg-gray-50/90 dark:bg-gray-50 disabled:opacity-50 shadow px-4 py-2 rounded-md h-9 font-medium text-gray-50 text-sm dark:text-gray-900 transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none dark:focus-visible:ring-gray-300"
+                  href="#"
+                >
+                  Get Started
+                </Link>
+              </div>
+              <div className="flex flex-col items-start space-y-4">
+                <div className="inline-block bg-gray-100 dark:bg-gray-800 px-3 py-1 rounded-lg text-sm">
+                  Security
+                </div>
+                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed dark:text-gray-400">
+                  Fully managed infrastructure designed to scale dynamically
+                  with your traffic, a global edge to ensure your site is fast
+                  for every customer, and the tools to monitor every aspect of
+                  your app.
+                </p>
+                <Link
+                  className="inline-flex justify-center items-center border-gray-200 dark:border-gray-800 bg-white hover:bg-gray-100 dark:hover:bg-gray-800 dark:bg-gray-950 disabled:opacity-50 shadow-sm px-4 py-2 border rounded-md h-9 font-medium text-sm hover:text-gray-900 transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 dark:hover:text-gray-50 disabled:pointer-events-none dark:focus-visible:ring-gray-300"
+                  href="#"
+                >
+                  Contact Sales
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      {/* <footer className="flex sm:flex-row flex-col items-center gap-2 px-4 md:px-6 py-6 border-t w-full shrink-0">
+        <p className="text-gray-500 text-xs dark:text-gray-400">
+          © 2024 Acme Inc. All rights reserved.
+        </p>
+        <nav className="flex gap-4 sm:gap-6 sm:ml-auto">
+          <Link className="text-xs underline-offset-4 hover:underline" href="#">
+            Terms of Service
+          </Link>
+          <Link className="text-xs underline-offset-4 hover:underline" href="#">
+            Privacy
+          </Link>
+        </nav>
+      </footer> */}
+    </div>
+  )
 }
